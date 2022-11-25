b8290f51ce15824cd671f1b6f81acd23fb78bf6e
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2022-11-25 09:16:57 +0100

    OP uppg 2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Skriv ett nummer");
+        int kryptnummer = Convert.ToInt32(Console.ReadLine());
+        if (kryptnummer <= 0)
+        {
+            Console.WriteLine("Fel! du måste skrive ett positivt heltal som inte är 0");
+        }
+        else if (kryptnummer >= 1 && kryptnummer <= 10)
+        {
+            Console.WriteLine("Det finns 1 möjlig bokstavskombination.");
+        }
+        else if (kryptnummer >= 11 && kryptnummer <=29 && kryptnummer != 20)
+        {
+            Console.WriteLine("Det finns 2 möjliga bokstavskombinationer.");
+        }
+        else if (kryptnummer == 20)
+        {
+            Console.WriteLine("Det finns 1 möjlig bokstavskombination.");
+        }
+        else if (kryptnummer >= 30 && kryptnummer <=99)
+        {
+            Console.WriteLine("Det finns 1 möjlig bokstavskombination.");
+        }
+        else 
+        {
+            Console.WriteLine("Siffran är för stor för min hjärna lol. Prova ett tal mellan 1 och 99");
+        }
+    }
+}
+
